f74198905f838f7c82ac7446627414ce2fb25b77
niloymondal <62515093+niloymondal@users.noreply.github.com>
2024-02-19 05:46:24 +0600

    Update Task2.php

--- Task-02/Task2.php
+++ Task-02/Task2.php
@@ -1,14 +1,14 @@
 <?php
 
-$apiKey = 'LVkT40AAbKcEoypKafIDefhIuIiqGt9g';
+$apiKey = 'LVkT40AAbKcEoypKafIDefhIuIiqGt9g'; //custom made api key from accuweather
 $cityKey = '28143';
 
-$url = "http://dataservice.accuweather.com/currentconditions/v1/{$cityKey}?apikey={$apiKey}";
+$url = "http://dataservice.accuweather.com/currentconditions/v1/{$cityKey}?apikey={$apiKey}"; //API Request URL
 $weatherData = json_decode(file_get_contents($url) , true);
 
 if ($weatherData)
 {
-    $temperature = $weatherData[0]['Temperature']['Imperial']['Value'];
+    $temperature = $weatherData[0]['Temperature']['Imperial']['Value']; //extracts temperature and weather text
     $weatherText = $weatherData[0]['WeatherText'];
 
    
