9b30ecff94e297c08f297c8827142831372750d0
niloymondal <62515093+niloymondal@users.noreply.github.com>
2024-02-19 23:39:12 +0600

    Add files via upload

---
+++ Additional/functions.php
@@ -0,0 +1,5 @@
+<?php
+function enqueue_custom_scripts() {
+    wp_enqueue_script('custom-script', get_template_directory_uri() . '/js/custom-script.js', array('jquery'), '1.0', true);
+}
+add_action('wp_enqueue_scripts', 'enqueue_custom_scripts');
---
+++ Additional/index.php
@@ -0,0 +1,8 @@
+<?php get_header(); ?>
+
+<div id="content">
+    <h2>Welcome to MERNS BD</h2>
+    <p>Content goes here...</p>
+</div>
+
+<?php get_footer(); ?>
